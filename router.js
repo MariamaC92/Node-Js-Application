1b8efe4e76198d3609bbc3f54f0066213a09ae08
MariamaC92 <122444096+MariamaC92@users.noreply.github.com>
2023-03-28 15:36:45 +0200

    second endpoint

--- router.js
+++ router.js
@@ -10,11 +10,24 @@ router.get("/", async (req, res) => {
  }
  
 catch (err) {
-    console.log(err.message);
+    // console.log(err.message);
     res.sendStatus(404)
 
  }
    
 });
+
+router.get("/api/:id", async (req,res) => {
+    const {id} = req.params;
+    try {
+        const {rows} = await pool.query ("SELECT * FROM users WHERE id=$1;", [id]);
+        res.json({data: rows});
+
+    } catch (err) { 
+        console.log(err.message)
+        res.sendStatus(500);}
+})
+
+
 
 module.exports = router;
