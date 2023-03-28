74c5fa0545994bf64042249a901b88e0303a0c67
MariamaC92 <122444096+MariamaC92@users.noreply.github.com>
2023-03-28 17:14:51 +0200

    last endpoint

--- router.js
+++ router.js
@@ -68,7 +68,7 @@ router.get("/orders",async (req, res) => {
     try{
         const {rows} = await pool.query("SELECT * FROM orders")
         res.json({data: rows});
-    } catch {
+    } catch (err){
         res.sendStatus(404);
     }
 })
@@ -90,6 +90,29 @@ router.post("/orders", async (req, res) => {
         res.json({data: rows});
     } catch (err){ res.sendStatus(404)}
 })
+
+router.put("/orders/:id", async (req, res) => {
+    try{
+        const {id} = req.params;
+        const {price} = req.body;
+        const {rows} = await pool.query("UPDATE orders SET price=$1 WHERE id=$2 RETURNING *" [price,id]);
+        res.send({data: rows})
+    } catch(err) {
+        res.sendStatus(404);
+    }
+})
+
+router.delete("/order/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+      const { rows } = await pool.query("DELETE FROM orders WHERE id=$1", [id]);
+      res.json({
+        message: `Order has been removed.`,
+      });
+    } catch (err) {
+      res.sendStatus(404);
+    }
+  });
 
 
 
